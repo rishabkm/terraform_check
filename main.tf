9fc0199400184aa56034a87f7192df4b185a144a
rishabkm <119279619+rishabkm@users.noreply.github.com>
2025-04-11 01:09:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,24 @@
 terraform {
   required_providers {
+    time = {
+      source = "hashicorp/time"
+    }
     local = {
-      source  = "hashicorp/local"
+      source = "hashicorp/local"
     }
   }
 }
 
+provider "time" {}
 provider "local" {}
 
+resource "time_offset" "ist_time" {
+  offset_hours   = 5
+  offset_minutes = 30
+  base_rfc3339   = timestamp()
+}
+
 resource "local_file" "example" {
-  content  = "This file was created by Terraform at ${timestamp()}"
+  content  = "This file was created by Terraform at ${time_offset.ist_time.rfc3339}"
   filename = "${path.module}/hello_from_terraform.txt"
 }
